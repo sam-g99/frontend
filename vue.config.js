84243ec50105d1362b8e14c824f97647c998ef48
Sam G <samuelgraham@protonmail.com>
2019-10-24 21:44:36 -0400

    fix favicon

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   configureWebpack: {
     plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        // any other options you'd normally set are still supported:
+        compile: false,
+        inject: true,
+      }),
+
       new PrerenderSPAPlugin({
         staticDir: path.join(__dirname, 'dist'),
         // Required - Routes to render. (EXAMPLE: routes: ['/', '/about', '/some/deep/nested/route']),
